157500cdcccb41b8ec1d7ac46867e13ac0e11bbe
Hunter Parks <hunter.thomas.parks@gmail.com>
2018-10-26 03:41:20 +0900

    refactor code to function

--- random_loot/SelectionWindow.xaml.cs
+++ random_loot/SelectionWindow.xaml.cs
@@ -27,14 +27,20 @@ namespace random_loot
 
         public void Window_Event(object sender, RoutedEventArgs e)
         {
+            rarityArray = SetRarityArray(rarityArray);
+            MainWindow window2 = new MainWindow(rarityArray, this);
+            window2.Show();
+        }
+
+        public List<string> SetRarityArray(List<string> rarityArray)
+        {
             rarityArray[0] = Mundane.IsChecked == true ? "Mundane" : null;
             rarityArray[1] = Common.IsChecked == true ? "Common" : null;
             rarityArray[2] = Uncommon.IsChecked == true ? "Uncommon" : null;
             rarityArray[3] = Rare.IsChecked == true ? "Rare" : null;
             rarityArray[4] = Legendary.IsChecked == true ? "Legendary" : null;
             rarityArray[5] = Misc.IsChecked == true ? "Misc" : null;
-            MainWindow window2 = new MainWindow(rarityArray, this);
-            window2.Show();
+            return rarityArray;
         }
     }
 }

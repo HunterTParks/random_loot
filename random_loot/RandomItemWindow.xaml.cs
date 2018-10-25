a91830f71e8df8791da2ddc64c95d431f1788db8
Hunter Parks <hunter.thomas.parks@gmail.com>
2018-10-26 04:20:26 +0900

    copy db connection from MainWindow
    This function is just copied over but will be repurposed to suit this
    window's needs; selecting random items

--- random_loot/RandomItemWindow.xaml.cs
+++ random_loot/RandomItemWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Data.SQLite;
 
 namespace random_loot
 {
@@ -19,9 +20,66 @@ namespace random_loot
     /// </summary>
     public partial class RandomItemWindow : Window
     {
-        public RandomItemWindow()
+        public RandomItemWindow(List<string> rarityArray)
         {
             InitializeComponent();
+            
         }
+
+        void DbConnection(List<string> rarityArray)
+        {
+            using (SQLiteConnection conn = new SQLiteConnection(@"Data Source=C:\Tools\Databases\loot.db;"))
+            {
+                string query = "SELECT * FROM loot_table WHERE ";
+                bool beginningOfQuery = true;
+
+                /*
+                 * Remove all null values from array
+                 * 
+                 */
+                for (int i = 0; i <= rarityArray.Count - 1; i++)
+                {
+                    if (rarityArray[i] == null)
+                        rarityArray.RemoveAt(i);
+                }
+
+                /*
+                 * Add Rarity values to SQL Query
+                 *
+                 */
+                for (int i = 0; i <= rarityArray.Count - 1; i++)
+                {
+                    if (i == 0 && beginningOfQuery)
+                        query += "Rarity = '" + rarityArray[i] + "'";
+                    else
+                    {
+                        query += " OR Rarity = '" + rarityArray[i] + "'";
+                    }
+                }
+
+                /*
+                 * Add semi-colon to end of query to keep SQL syntax
+                 * 
+                 */
+                query += ";";
+
+                /*
+                 * Opens connection to local Database and posts data to datagrid
+                 * 
+                 */
+                conn.Open();
+
+                SQLiteCommand command = new SQLiteCommand(query, conn);
+                using (SQLiteDataAdapter dataAdapter = new SQLiteDataAdapter(command))
+                {
+                    System.Data.DataTable dataTable = new System.Data.DataTable();
+                    dataAdapter.Fill(dataTable);
+                    Grid.ItemsSource = dataTable.DefaultView;
+                }
+
+                conn.Close();
+            }
+        }
+
     }
 }

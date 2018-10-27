c0bf4d247760a90f17ad817c7b9a731c29e2e945
Hunter Parks <hunter.thomas.parks@gmail.com>
2018-10-27 14:06:17 +0900

    add properties to Item class

--- random_loot/Item.cs
+++ random_loot/Item.cs
@@ -8,5 +8,16 @@ namespace random_loot
 {
     class Item
     {
+        int Id { get; set; }
+        string Name { get; set; }
+        string Description { get; set; }
+        string Value { get; set; }
+        string Rarity { get; set; }
+        string Weight { get; set; }
+        string Category { get; set; }
+        string Properties { get; set; }
+        string Requirements { get; set; }
+        string Author { get; set; }
+        string RarityNumber { get; set; }
     }
 }

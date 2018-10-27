600cd81f423c7df6f49eff5785941ec348f65bee
Hunter Parks <hunter.thomas.parks@gmail.com>
2018-10-27 14:24:50 +0900

    add getters, setters, and constructor to Item class

--- random_loot/Item.cs
+++ random_loot/Item.cs
@@ -8,6 +8,21 @@ namespace random_loot
 {
     class Item
     {
+        public Item(int id, string name, string description, string value, string rarity, string weight, string category, string properties, string requirements, string author, string rarityNumber)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            Value = value;
+            Rarity = rarity;
+            Weight = weight;
+            Category = category;
+            Properties = properties;
+            Requirements = requirements;
+            Author = author;
+            RarityNumber = rarityNumber;
+        }
+
         int Id { get; set; }
         string Name { get; set; }
         string Description { get; set; }
